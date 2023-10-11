b054b3559c2cc3c6d6fad1bfd9e7eb22b9941f98
Ajmal-Ali-k <ajmalali749@gmail.com>
2023-10-11 23:32:28 +0530

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage (){
+    return(
+        <div className="flex justify-between items-center">
+        <h1 className="text-center font-bold text-2xl">hello login</h1>
+        </div>
+    )
+}
